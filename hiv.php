3a1aefc674cc00eef9852569a3d1fdeebb79f2e9
ArtSyntax <arttra_eagleb@hotmail.com>
2015-07-22 17:09:33 +0900

    add bootstrap

--- hiv.php
+++ hiv.php
@@ -1,16 +1,21 @@
-
 <html>
 
 	<head>
 	<title>Health Visualization</title>
 		<meta http-equiv="Content-Language" content="English" />
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+   
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  
 		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
 		
 		<script src="ammap/ammap.js" type="text/javascript"></script>
 		<link rel="stylesheet" href="ammap/ammap.css" type="text/css" media="all" />
 		<script src="ammap/maps/js/worldLow.js" type="text/javascript"></script>
-		
+			
 		<?php
 			header("Content-Type: text/html; charset=UTF-8");
 			 
@@ -43,54 +48,55 @@
 	</head>
 	
 	<body>
+		<div class="container">
+			<div id="wrap" >
 			
-			
-		<div id="wrap">
-
-			<div id="header">
-				<h1><a href="index.php">Health Visualization</a></h1>
-				<h2>Nagahama Institute of Bio-Science and Technology</h2>
-			</div>
-
-			<div id="right">
-
-				<h2><a href="#">Prevalence of HIV, total (% of population ages 15-49) - 2013</a></h2>
-				<div class="articles">
-					
-					<br />
-											
-					<center><div id="mapdiv" style="width: 100%; height: 70%; background-color:#FAFAFA;"></div></center>
-					<i>
-						<br>Note 1 - Grey countries are the countries without data. 
-						<br>Note 2 - The country doesn't have a value. It mean the value is less than one. 
-					</i>
-					<br /><br />
-					
-					Prevalence of HIV refers to the percentage of people ages 15-49 who are infected with HIV.
-					<br> UNAIDS estimates.
-					<br><br>					
+				<div id="header" >
+					<h1><a href="index.php">Health Visualization</a></h1>
+					<h2>Nagahama Institute of Bio-Science and Technology</h2>
 				</div>
-			</div>
-
-			<div id="left"> 
-
-				<h3>Categories </h3>
-				<ul>
-				<li><a href="main.php">Home</a></li> 
-				<li><a href="pop_total.php">Population Total</a></li>
-				<li><a href="pop_growth.php">Population Growth</a></li>
-				<li><a href="age.php">Life Expectancy</a></li> 
-				<li><a href="death_rate.php">Death Rate</a></li> 
-				<li><a href="hiv.php">Prevalence of HIV</a></li> 
-				<li><a href="reference.php">Reference</a></li> 
-				</ul>
-
-			</div>
-			<div style="clear: both;"> </div>
-
-			<div id="footer">
-				Designed by <a href="https://www.facebook.com/pages/Art-Thunder/749793481806968?ref=tn_tnmn" target="blank">Art Thunder.</a> Internship Health Visualization Project.
-				<br>Copyright <a href="http://www.nagahama-i-bio.ac.jp/" target="blank">Nagahama Institute of Bio-Science and Technology.</a> All rights Reserved.	2015			
+				<div class="row">
+					<div class="col-sm-3">
+						<h3>Categories </h3>
+						<div id="left"> 
+							<ul>
+							<li><a href="main.php">Home</a></li> 
+							<li><a href="pop_total.php">Population Total</a></li>
+							<li><a href="pop_growth.php">Population Growth</a></li>
+							<li><a href="age.php">Life Expectancy</a></li> 
+							<li><a href="death_rate.php">Death Rate</a></li> 
+							<li><a href="hiv.php">Prevalence of HIV</a></li> 
+							<li><a href="reference.php">Reference</a></li> 
+							</ul>
+						</div>
+					</div>
+					
+					<div class="col-sm-9">
+						<div id="right"> 
+							<h2><a href="#">Prevalence of HIV, total (% of population ages 15-49) - 2013</a></h2>
+							<div class="articles">
+								
+								<br />
+								<center><div id="mapdiv" style="width: 100%; height: 70%; background-color:#FAFAFA;"></div></center>
+								
+								<i>
+									<br>Note 1 - Grey countries are the countries without data. 
+									<br>Note 2 - The country doesn't have a value. It mean the value is less than one. 
+								</i>
+								<br /><br />
+					
+								Prevalence of HIV refers to the percentage of people ages 15-49 who are infected with HIV.
+								<br> UNAIDS estimates.
+								<br><br>					
+							</div>
+						</div>
+					</div>
+				</div>					
+		
+				<div id="footer">
+					Designed by <a href="https://www.facebook.com/pages/Art-Thunder/749793481806968?ref=tn_tnmn" target="blank">Art Thunder.</a> Internship Health Visualization Project.
+					<br>Copyright <a href="http://www.nagahama-i-bio.ac.jp/" target="blank">Nagahama Institute of Bio-Science and Technology.</a> All rights Reserved.	2015			
+				</div>
 			</div>
 		</div>
 	</body>
--- reference.php
+++ reference.php
@@ -4,90 +4,99 @@
 		<title>Health Visualization</title>
 		<meta http-equiv="Content-Language" content="English" />
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+   
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  
 		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
 		
 		<style>
 			#slidy-container {width: 60%; height: 100%; overflow: hidden;			}
 		</style>
-
-		
 	</head>
 	
-	
 	<body>
+		<div class="container">
+			<div id="wrap" >
 			
-			
-		<div id="wrap">
-
-			<div id="header">
-				<h1><a href="index.php">Health Visualization</a></h1>
-				<h2>Nagahama Institute of Bio-Science and Technology</h2>
-			</div>
-
-			<div id="right">
-
-				<h2><a href="#">Reference</a></h2>
-				<div class="articles">
-					Database from <a href="http://www.worldbank.org/" target="_blank">The World Bank.</a> <br>
-					World Bank Open Data: free and open access to data about development in countries around the globe.<br>
-					The World Bank. (2015). Health. Retrieved from 
-					<a href="http://data.worldbank.org/indicator?display=default" target="_blank" >http://data.worldbank.org/indicator?display=default</a>
-
-					<br /><br />
-						
-					<center><a href="http://www.worldbank.org/" target="_blank">
-						<img src="img/world-bank-logo.jpg" height="100%" width="20%">
-						</a></center>
-
-					<br><br><br>	
-						
-					<h2><a href="#">Manipulator</a></h2>
-					Project by <a href="http://www.nagahama-i-bio.ac.jp/" target="blank">Nagahama Institute of Bio-Science and Technology.</a><br>
-					Examiner: Prof. Hiroshi Nagata<br>
-					Developer: Mr.Thanyavuth Akarasomcheep (Art Thunder)
-					
-					<br /><br />
-					
-					<center><a href="http://www.nagahama-i-bio.ac.jp/" target="_blank">
-						<img src="img/nbio_logo.gif" height="100%" width="20%">
-						</a></center>	
-					 
-					<br /><br /><br />
-					
-					<center>		
-					<div id="slidy-container">
-						<figure id="slidy">
-							<img src="img/World_Bank_Headquarters.jpg" alt="The World Bank">							
-							<img src="img/nbio.jpg" alt="Nagahama Institute of Bio-Science and Technology">
-						</figure>
-					</div>
-					</center>
-						
-					<br /><br /><br /><br /><br />
-				
-					
+				<div id="header" >
+					<h1><a href="index.php">Health Visualization</a></h1>
+					<h2>Nagahama Institute of Bio-Science and Technology</h2>
 				</div>
-			</div>
+				<div class="row">
+					<div class="col-sm-3">
+						<h3>Categories </h3>
+						<div id="left"> 
+							<ul>
+							<li><a href="main.php">Home</a></li> 
+							<li><a href="pop_total.php">Population Total</a></li>
+							<li><a href="pop_growth.php">Population Growth</a></li>
+							<li><a href="age.php">Life Expectancy</a></li> 
+							<li><a href="death_rate.php">Death Rate</a></li> 
+							<li><a href="hiv.php">Prevalence of HIV</a></li> 
+							<li><a href="reference.php">Reference</a></li> 
+							</ul>
+						</div>
+					</div>
+					
+					<div class="col-sm-9">
+						<div id="right"> 
+							<h2><a href="#">Reference</a></h2>
+							<div class="articles">
+								Database from <a href="http://www.worldbank.org/" target="_blank">The World Bank.</a> <br>
+								World Bank Open Data: free and open access to data about development in countries around the globe.<br>
+								The World Bank. (2015). Health. Retrieved from 
+								<a href="http://data.worldbank.org/indicator?display=default" target="_blank" >http://data.worldbank.org/indicator?display=default</a>
+								<br /><br />
+									
+								<center>
+									<a href="http://www.worldbank.org/" target="_blank">
+										<img src="img/world-bank-logo.jpg" height="100%" width="20%">
+									</a>
+								</center>
 
-			<div id="left"> 
-
-				<h3>Categories </h3>
-				<ul>
-				<li><a href="main.php">Home</a></li> 
-				<li><a href="pop_total.php">Population Total</a></li>
-				<li><a href="pop_growth.php">Population Growth</a></li>
-				<li><a href="age.php">Life Expectancy</a></li> 
-				<li><a href="death_rate.php">Death Rate</a></li> 
-				<li><a href="hiv.php">Prevalence of HIV</a></li> 
-				<li><a href="reference.php">Reference</a></li> 
-				</ul>
-
-			</div>
-			<div style="clear: both;"> </div>
-
-			<div id="footer">
-				Designed by <a href="https://www.facebook.com/pages/Art-Thunder/749793481806968?ref=tn_tnmn" target="blank">Art Thunder.</a> Internship Health Visualization Project.
-				<br>Copyright <a href="http://www.nagahama-i-bio.ac.jp/" target="blank">Nagahama Institute of Bio-Science and Technology.</a> All rights Reserved.	2015			
+								<br><br><br>	
+							</div>
+							
+							<h2><a href="#">Manipulator</a></h2>
+							<div class="articles">
+								Project by <a href="http://www.nagahama-i-bio.ac.jp/" target="blank">Nagahama Institute of Bio-Science and Technology.</a><br>
+								Examiner: Prof. Hiroshi Nagata<br>
+								Developer: Mr.Thanyavuth Akarasomcheep (Art Thunder)
+								<br /><br />
+								
+								<center>
+									<a href="http://www.nagahama-i-bio.ac.jp/" target="_blank">
+										<img src="img/nbio_logo.gif" height="100%" width="20%">
+									</a>
+								</center>	
+					 
+								<br /><br /><br />
+							</div>
+							
+							<center>
+								<div id="slidy-container">
+									
+										<figure id="slidy">
+											
+												<img src="img/World_Bank_Headquarters.jpg" alt="The World Bank">							
+												<img src="img/nbio.jpg" alt="Nagahama Institute of Bio-Science and Technology">
+												
+										</figure>
+									
+								</div>
+							</center>
+							<br /><br /><br /><br /><br />
+						</div>
+					</div>
+				</div>
+				
+				<div id="footer">
+					Designed by <a href="https://www.facebook.com/pages/Art-Thunder/749793481806968?ref=tn_tnmn" target="blank">Art Thunder.</a> Internship Health Visualization Project.
+					<br>Copyright <a href="http://www.nagahama-i-bio.ac.jp/" target="blank">Nagahama Institute of Bio-Science and Technology.</a> All rights Reserved.	2015			
+				</div>
 			</div>
 		</div>
 	</body>
