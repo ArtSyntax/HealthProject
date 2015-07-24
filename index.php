322bbb477161bc46b0e6c194c5e41c9d32a3a4f9
ArtSyntax <arttra_eagleb@hotmail.com>
2015-07-24 15:21:58 +0900

    public v1

--- index.php
+++ index.php
@@ -1,50 +1,43 @@
 <!DOCTYPE html>
 <html>
-<head>
-   	<title>Health Visualization</title>
-	<meta http-equiv="Content-Language" content="English" />
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
-	
-	<script src="js/zoomSlider.js" type="text/javascript"></script>
-    <link href="css/zoomslider.css" rel="stylesheet" type="text/css" />
-    <style type="text/css">
-        body {background:#333;padding-top:0px;font:normal 12px Arial;}
-        .nav {width:840px; margin:0 auto;padding-top:40px;}
-        .nav a {color:#09C;}
-    </style>
-</head>
-<body>
-	
-	<div id="wrapper">
-		<div id="homehead">
-			<h1><a href="#">Health Visualization</a></h1>
-		</div>
-        <div id="zoom-slider">
-			<img src="img/home2.jpg" alt="Run for your lives">
-			<img src="img/home3.jpg" alt="Rest a while and run a mile">
-			<img src="img/home4.jpg" alt="Recover your mind and body">
-			<img src="img/home1.jpg" alt="Commit to be Fit">
-			
+	<head>
+		<title>Health Visualization</title>
+		<meta http-equiv="Content-Language" content="English" />
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+		
+		<script src="js/zoomSlider.js" type="text/javascript"></script>
+		<link href="css/zoomslider.css" rel="stylesheet" type="text/css" />
+		<style type="text/css">
+			body {background:#333;padding-top:0px;font:normal 12px Arial;}
+			.nav {width:840px; margin:0 auto;padding-top:40px;}
+			.nav a {color:#09C;}
+		</style>
+	</head>
+
+	<body>
+		<div id="wrapper">
+			<div id="homehead">
+				<h1><a href="#">Health Visualization</a></h1>
+			</div>
+			<div id="zoom-slider">
+				<img src="img/home2.jpg" alt="Run for your lives">
+				<img src="img/home3.jpg" alt="Rest a while and run a mile">
+				<img src="img/home4.jpg" alt="Recover your mind and body">
+				<img src="img/home1.jpg" alt="Commit to be Fit">
+			</div>
+			<div id="dim">
+				<img src="img/dim.jpg" width=100%>
+			</div>
 		</div>
 		
-		<div id="dim">
-			<img src="img/dim.jpg" width=100%>
+		<div id="link" >
+			<center>
+				<a href="main.php"><img src="img/home_button2.png" 
+				onMouseOver="this.src='img/home_button.png'" 
+				onMouseOut="this.src='img/home_button2.png'"
+				width="70 px"/></a>
+			</center>
 		</div>
-				
-    </div>
-	
-
-	
-	
-	<div id="link" >
-		<center>
-			<a href="main.php"><img src="img/home_button2.png" 
-			onMouseOver="this.src='img/home_button.png'" 
-			onMouseOut="this.src='img/home_button2.png'"
-			width="70 px"/></a>
-		</center>
-	</div>
-
-</body>
+	</body>
 </html>
